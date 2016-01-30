147755cdf05b89b4bbb5a5186f2ee20463f56014
juherask <jussi.rasku@jyu.fi>
2016-01-30 15:38:23 +0200

    - Fixed tabs, fixed non-working color replacement on 32/64 bit.

--- toy.py
+++ toy.py
@@ -1,7 +1,13 @@
 import pygame
 from random import random, randint
 from collections import namedtuple
-from numpy import int32
+from numpy import int32, int64
+
+import platform
+if platform.architecture()[0]=='32bit':
+    color_bit_conversion = int64
+else:
+    color_bit_conversion = int32
 
 # ~DEFINES
 X = 0
@@ -22,6 +28,7 @@ def recolor_sprite(sprite):
     #print hex(pixels[0][0] & (2**32-1))
     #print hex(pixels[15][25] & (2**32-1))
     #print hex(pixels[25][15] & (2**32-1))
+    #print(pixels[15][25], hex(pixels[15][25]))
     
     a = 255
     r = randint(70,200)
@@ -30,17 +37,26 @@ def recolor_sprite(sprite):
     light_shirt_color = a*256**3+r*256**2 + g*256 + b
     dark_shirt_color = a*256**3+(r-50)*256**2 + (g-50)*256 + (b-50)
     
+<<<<<<< .mine
+    for x in range(len(pixels)):
+        for y in range(len(pixels[0])):
+            if pixels[x][y]==color_bit_conversion(0xFF8E524A):
+                pixels[x][y] = color_bit_conversion(light_shirt_color)
+            if pixels[x][y]==color_bit_conversion(0xFF603833):
+                pixels[x][y] = color_bit_conversion(dark_shirt_color)
+=======
     for x in xrange(len(pixels)):
         for y in xrange(len(pixels[0])):
 	    if pixels[x][y]==int32(0xFF8E524A):
                 pixels[x][y] = int32(light_shirt_color)
 	    if pixels[x][y]==int32(0xFF603833):
 		pixels[x][y] = int32(dark_shirt_color)
+>>>>>>> .r9
 
 def update(units):
     for unit in units:       
-	unit.position[X] = unit.position[X]+randint(-1,1) 
-	unit.position[Y] = unit.position[Y]+randint(-1,1)
+        unit.position[X] = unit.position[X]+randint(-1,1) 
+        unit.position[Y] = unit.position[Y]+randint(-1,1)
         
 def draw(screen, units):
     global man_sprite
@@ -48,15 +64,15 @@ def draw(screen, units):
     screen.fill((255, 255, 255)) 
     
     for unit in units:
-	screen.blit(unit.sprite, unit.position)
+        screen.blit(unit.sprite, unit.position)
     
     pygame.display.update()
     
     
 def handle_input_events():
     for event in pygame.event.get():
-	if event.type == pygame.QUIT:
-	    on_esc_pressed()
+        if event.type == pygame.QUIT:
+            on_esc_pressed()
 
 def on_esc_pressed():
     global done
@@ -73,14 +89,14 @@ load_assets()
 
 ## Variable initialization ##
 units = []
-for i in xrange(100):
+for i in range(100):
     unit_man_sprite = man_sprite.copy()
     recolor_sprite(unit_man_sprite)
     
     new_unit = Unit(
-	position=[randint(0,size[X])-30, randint(0,size[Y])-50],
-	sprite=unit_man_sprite,
-	state = None) #state undefined for now
+    position=[randint(0,size[X])-30, randint(0,size[Y])-50],
+    sprite=unit_man_sprite,
+    state = None) #state undefined for now
     units.append( new_unit )
 
     
@@ -100,7 +116,7 @@ while done == False:
     # run at X fps
     clock.tick(5)
     
-    pygame.image.save(screen, "screenshot%02d.tga" % nloop)
+    #pygame.image.save(screen, "screenshot%02d.tga" % nloop)
     nloop+=1
  
 # close the window and quit
